8cada75036400278173ce7389d7518516607cc9d
aamirrasheed <aamir1rasheed@gmail.com>
2019-09-11 19:36:28 -0700

    Modified shit

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd ledsquare
-git pull
 sudo ./rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=20 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper=Rotate:-90 -f -w1 finaldisplay/frame_1* finaldisplay/frame_2*
+echo 'Running Daniels LED square!'
