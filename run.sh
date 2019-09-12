24672e0f2d8301635b88ab1845c25545006d40d8
aamirrasheed <aamir1rasheed@gmail.com>
2019-09-11 19:41:13 -0700

    Added new frame to artist point sequence, modified run.sh to read all frames with frame*

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Running Daniels LED square!'
-sudo ./rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=20 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper=Rotate:-90 -f -w1 finaldisplay/frame_1* finaldisplay/frame_2*
+sudo ./rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=20 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper=Rotate:-90 -f -w1 finaldisplay/frame*
