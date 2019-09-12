b99d625e761b1d61c8d1dc0d93d822f6bc240997
aamirrasheed <aamir1rasheed@gmail.com>
2019-09-12 01:20:50 -0700

    changed again

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Running Daniels LED square!'
-sudo ./rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=20 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper=Rotate:-90 -f -w1 finaldisplay/frame*
+sudo ./rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=20 --led-pwm-lsb-nanoseconds=50 --led-pixel-mapper=Rotate:-90 -f -w3 finaldisplay/frame*
